0a607a3a495f56144baea7417092e9c3ad9a459e
wardamukhtarkhan <102410054+wardamukhtarkhan@users.noreply.github.com>
2022-05-10 18:46:45 +0500

    Add files via upload

---
+++ or gate.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num=5,no=2;
+    
+    if ((num==5) || (num==3))
+    {
+        cout<<"ok";
+    }
+    else if (num==10)
+    {
+        cout<< "not ok";
+    }
+    else
+    {
+        cout<< "error";
+    }
+   
+    return 0;
+}
